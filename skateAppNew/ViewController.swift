196bcdc674e73991a7e21b216d706ce03be820ae
Khanh <323.kvn@gmail.com>
2020-07-15 15:03:54 -0700

    test2

---
+++ skateAppNew/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  skateAppNew
+//
+//  Created by Khanh Nguyen on 7/15/20.
+//  Copyright © 2020 Khanh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+//hello
